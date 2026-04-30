0a1b77729f3fb657a6f7134bcf69ab208bdc9876
krisnokumarghosh <krishnokumarghosh112@gmail.com>
2026-04-30 22:10:11 +0600

    added tile details page

--- src/app/all-tiles/[id]/page.jsx
+++ src/app/all-tiles/[id]/page.jsx
@@ -12,23 +12,38 @@ const TileDetailsPage = async({params}) => {
     const tile = tiles.find(item => item.id === id);
 
     return (
-        <div className='mt-27.5 lg:flex gap-20 lg:pl-120'>
+        <div className='my-27.5 lg:flex gap-20 lg:pl-120'>
            <div>
             <Image
             alt='tile image'
             height={500}
             width={400}
             src={tile.image}
-            className='w-100 h-140 mx-auto lg:mx-0'
+            className='w-65 h-85 md:w-100 md:h-140 mx-auto lg:mx-0'
             ></Image>
            </div>
 
            <div className='text-center lg:text-left'>
-            <h1 className={`${NotoSerifFont.className} text-[35px] text-[#1E1E1E]`}>{tile.title}</h1>
+            <h1 className={`${NotoSerifFont.className} mt-4 lg:mt-0 text-[28px] md:text-[35px] text-[#1E1E1E]`}>{tile.title}</h1>
             <p className='lg:text-left mt-3 font-semibold text-[#1E1E1E]'>{tile.category}</p>
-            <div className="divider w-114 text-[#78716C] my-10 mx-auto lg:mx-0"></div>
-            <p className='text-[13px] text-[#9E9B98] mb-4'>DESCRIPTION</p>
-            <p></p>
+            <h3 className='mt-3 text-[20px] font-bold text-[#1E1E1E]'>
+                ${tile.price}
+            </h3>
+            <div className="divider md:w-114 text-[#78716C] my-10 mx-auto lg:mx-0"></div>
+            <p className='text-[13px] text-[#9E9B98] mb-4 font-semibold'>DESCRIPTION</p>
+            <p className='w-65  md:w-114 mx-auto lg:mx-0  leading-7 text-[#444748]'>{tile.description}</p>
+            <p className='text-[13px] mt-10 text-[#9E9B98] mb-4 font-semibold'>SPECIFICATIONS</p>
+           <div className='space-y-3'>
+             <div className='flex justify-center gap-20 lg:justify-between items-center'>
+                <p className='text-[#9E9B98]'>Dimensions</p>
+                <p className='font-semibold'>{tile.dimensions}</p>
+            </div>
+            <div className='flex justify-center gap-20 lg:justify-between items-center'>
+                <p className='text-[#9E9B98]'>Material</p>
+                <p className='font-semibold'>{tile.material}</p>
+            </div>
+           </div>
+           <button className='btn border-0 mt-5 rounded-full bg-[#536257] lg:w-full text-white'>Request A Sample</button>
            </div>
         </div>
     );
